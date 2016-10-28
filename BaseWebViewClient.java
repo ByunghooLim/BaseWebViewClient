003f13ad96eb5d516a27af05bd5cfbaeafd2a481
Byunghoo Lim <byunghoolim@gmail.com>
2016-10-28 19:34:19 +0900

    Add files via upload
    First commit

---
+++ BaseWebViewClient.java
@@ -0,0 +1,30 @@
+package global.wemakeprice.com.basemodule;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by ByunghooLim on 2016. 10. 28..
+ * Custom WebView Client
+ */
+
+public abstract class BaseWebViewClient extends WebViewClient {
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        LogCat.i("URL", url);
+        return customShouldOverrideUrlLoading(view, url);
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        LogCat.i("URL", request.getUrl().toString());
+        return customShouldOverrideUrlLoading(view, request.getUrl().toString());
+    }
+
+    protected abstract boolean customShouldOverrideUrlLoading(WebView view, String url);
+}
